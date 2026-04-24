9cf7a3419d4486aac3a9e4cadedbab51c5326326
Kelvin Zimmerman <71656069+afterglow79@users.noreply.github.com>
2026-04-24 16:30:10 +0000

    set up text generation to fully generate story, save to text file, and clean.

--- gen/gentext.py
+++ gen/gentext.py
@@ -1,4 +1,7 @@
 import requests, base64
+import json
+import re
+
 invoke_url = "https://integrate.api.nvidia.com/v1/chat/completions"
 stream = False
 from gen.tts import begin as tts_begin
@@ -7,14 +10,40 @@ def read_b64(path):
   with open(path, "rb") as f:
     return base64.b64encode(f.read()).decode()
 
+def cleanFile():
+    with open("response.txt", "r") as f:
+        content = f.read()
+        cleaned_content = re.sub(r'\n{2,}', '\n', content)
+
+    with open("response.txt", "w") as f:
+        f.write(cleaned_content)
+
 def saveResponse(headers, payload):
     response = requests.post(invoke_url, headers=headers, json=payload, stream=stream)
-    if stream:
-        for line in response.iter_lines():
-            if line:
-                print(line.decode("utf-8"))
-    else:
-        print(response.json())
+    stillGoing = True
+    while(stillGoing):
+        if response.status_code != 200:
+            print("Error: " + str(response.status_code) + " - " + response.text)
+            break
+
+        if stream:
+            for line in response.iter_lines():
+                if line:
+                    print(line.decode("utf-8"))
+        else:
+            print(response.json())
+            stillGoing = False
+            break
+        
+    
+    response_data = response.json()
+    with open("response.txt", "w") as f:
+        with open("response.json", "w") as j:
+            data = response_data["choices"][0]["message"]["content"]
+            cleaned_data = re.sub(r'\n{2,}', '\n', data)
+            f.write(cleaned_data)
+            json.dump(response_data, j)
+
 def next(NVIDIA_API_KEY):
     tts_begin(NVIDIA_API_KEY)
 
@@ -29,20 +58,30 @@ def run(NVIDIA_API_KEY):
     "model": "qwen/qwen3.5-122b-a10b",
     "messages": [{"role":"user","content":
                     "Follow these instructions carefully: " +
-                    "You are to generate an entertaining reddit AITA story. " +
-                    "Make it as believable or unbelievable as you want. " +
-                    "However, you must follow some specific rules. "
-                    "First: You must NOT generate a title or original poster. " +
-                    "Second: You are to denote when the story starts by saying \"STORY_BEGIN\" exactly as typed." +
+                    "You are to generate an entertaining Reddit Am I the Asshole (AITA) story. " +
+                    "Make it as believable or unbelievable as you want, be sure to make it unique. " +
+                    "It must remain grounded in realism, however. " +
+                    "That is not say, nothing supernatural, for example. It should be a tad extreme. " +
+                    "However, you MUST follow some specific rules: \n " +
+                    "First: You must NOT generate a title or original poster. \n" +
+                    "Second: You are to denote when the story starts by saying \"STORY_BEGIN\" exactly as typed.\n" +
                     "Third: You do not have to include post comments, but you can. If you do, you must first list " +
-                    "the user (i.e \"u/exampleusername\"), and then the comment" +
-                    "You must denote when the FIRST comment starts by saying \"COMMENT_BEGIN\" exactly as I have."}],
+                    "the user (i.e \"u/exampleusername\"), and then the comment. " + 
+                    "You MUST come up with convincing usernames for the commentors, something that sounds real. " +
+                    "The comments must also be realistic.\n" +
+                    "Fourth: You must denote when the FIRST comment starts by saying \"COMMENT_BEGIN\" exactly as I have. \n\n" +
+                    "Now please generate the story following the above instructions. " +
+                    "Remember to follow the instructions carefully and to denote the " +
+                    "beginning of the story and comments as specified."
+                    }],
     "max_tokens": 16384,
     "temperature": 0.60,
     "top_p": 0.95,
     "stream": stream,
-    "chat_template_kwargs": {"enable_thinking":True},
+    "chat_template_kwargs": {"enable_thinking":False},
     }
+   print("Generating story...")
    saveResponse(headers, payload)
-   next(NIVIDA_API_KEY)
+   print("Done generating story. Moving on to TTS...")
+   next(NVIDIA_API_KEY)
 
